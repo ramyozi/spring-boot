d9873ad6df6ab0ac8a574cc010e19ddf78800bf3
Ramzi <ramzibenmansour.pro@gmail.com>
2024-04-09 14:57:25 +0200

    Tp-4 terminé

--- DigiHello/src/main/java/fr/diginamic/controllers/CityController.java
+++ DigiHello/src/main/java/fr/diginamic/controllers/CityController.java
@@ -1,24 +1,49 @@
 package fr.diginamic.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.diginamic.models.City;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cities")
 public class CityController {
+	
+	private final List<City> cities = new ArrayList<>();
 
+    public CityController() {
+        // Initialisation avec quelques villes à des fins de test
+        cities.add(new City("Paris", 2148000));
+        cities.add(new City("Lyon", 513275));
+    }
+
+    // Ma requete de fetch
     @GetMapping
-    public List<City> getVilles() {
-        return Arrays.asList(
-            new City("Paris", 2148000),
-            new City("Lyon", 513275),
-            new City("Marseille", 861635)
-        );
+    public List<City> getCities() {
+        return cities;
+    }
+
+    // Requete de création
+    @PostMapping
+    public ResponseEntity<String> addCity(@RequestBody City newCity) {
+        Optional<City> existingCity = cities.stream().filter(city -> city.getName().equalsIgnoreCase(newCity.getName())).findAny();
+        if (existingCity.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La ville existe déjà");
+        } else {
+            cities.add(newCity);
+            return ResponseEntity.ok("Ville insérée avec succès");
+        }
     }
 }
+
+
