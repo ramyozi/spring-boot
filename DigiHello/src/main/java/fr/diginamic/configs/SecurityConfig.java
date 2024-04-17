598bf3e5646e87c84d06f755be819fc9cef0dd73
Ramzi <ramzibenmansour.pro@gmail.com>
2024-04-17 12:23:23 +0200

    TP: Setup SecurityConfig

---
+++ DigiHello/src/main/java/fr/diginamic/configs/SecurityConfig.java
@@ -0,0 +1,32 @@
+package fr.diginamic.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+		userDetailsManager.createUser(
+				User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("user")
+				.roles("USER")
+				.build());
+		userDetailsManager.createUser(
+				User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("admin")
+				.roles("ADMIN")
+				.build()
+				);
+
+		return userDetailsManager;
+	}
+}
